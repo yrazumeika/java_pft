89d25c90456e085b281678d29e0e8370d3466b62
yrazumeika <yrazumeika@exadel.com>
2018-07-26 16:21:19 +0300

    update

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,5 @@
 public class MyFirstProgram {
 
-public static void main (String[] args) {
+public static void main (String[] args) { System.out.println("Hello, world!"); }
 
-System.out.println("Hello, world!");
-}
 }
